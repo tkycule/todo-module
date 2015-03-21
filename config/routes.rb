c0f78a7862eb1956f49138a16af79f9cfbc16455
tkycule <tky.cule@gmail.com>
2015-03-21 23:40:12 +0900

    sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
