6ed2a597b8fae08aefdb243263b510f2d72c9dc7
tkycule <tky.cule@gmail.com>
2015-03-22 01:28:22 +0900

    タスク

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,74 +1,82 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
+  before_action :set_count, only: [:index, :completed, :deleted]
+  before_action :set_task, only: [:edit, :update, :destroy, :complete, :revert]
+  before_action :set_new_task, only: [:index, :completed, :deleted]
+
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.inbox.order("id desc")
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
+  def completed
+    @tasks = current_user.tasks.completed.order("id desc")
+    render :index
   end
 
-  # GET /tasks/new
-  def new
-    @task = Task.new
+  def deleted
+    @tasks = current_user.tasks.deleted.order("id desc")
+    render :index
   end
 
-  # GET /tasks/1/edit
   def edit
+    if session[:task_params]
+      @task.attributes = session[:task_params]
+      @task.valid?
+      session.delete(:task_params)
+    end
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
-    @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: '作成しました。'
+    else
+      redirect_to tasks_path, alert: "入力してください。"
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: '更新しました。' 
+    else
+      flash[:alert] = '入力エラーがあります。'
+      session[:task_params] = task_params
+      redirect_to edit_task_path
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
-    @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @task.delete!
+    redirect_to :back, notice: 'ゴミ箱に入れました。'
+  end
+
+  def complete
+    @task.complete!
+    redirect_to :back, notice: "完了にしました。"
+  end
+
+  def revert
+    @task.revert!
+    redirect_to :back, notice: "収集箱に戻しました。"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:user_id, :title, :memo, :aasm_state)
-    end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :memo)
+  end
+
+  def set_new_task 
+    @new_task = Task.new
+  end
+
+  def set_count
+    @inbox_count = current_user.tasks.inbox.count
+    @completed_count = current_user.tasks.completed.count
+    @deleted_count = current_user.tasks.deleted.count
+  end
 end
--- app/views/tasks/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@tasks) do |task|
-  json.extract! task, :id, :user_id, :title, :memo, :aasm_state
-  json.url task_url(task, format: :json)
-end
