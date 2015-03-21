2b9fdb10a3d95b2f4759ff9d4576a19dd8421db4
tkycule <tky.cule@gmail.com>
2015-03-21 18:56:44 +0900

    appliation.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,29 @@ module TodoModule
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Set timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # 日本語化
+    I18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    # generatorの設定
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework  :rspec, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.view_specs false
+      g.controller_specs true
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs true
+      g.assets false
+      g.helper false
+    end
   end
 end
