b37e9e7600ca47912c63a88089c24126a89426e0
tkycule <tky.cule@gmail.com>
2015-03-21 23:53:07 +0900

    layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type.to_sym
+    when :success
+      "alert-success" # Green
+    when :error
+      "alert-danger" # Red
+    when :alert
+      "alert-warning" # Yellow
+    when :notice
+      "alert-info" # Blue
+    else
+      flash_type.to_s
+    end
+  end
+
+  def nav_link(link_path, options = nil, &block)
+
+    class_name = current_page?(link_path) ? 'current' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_path, options, &block
+    end
+  end
 end
