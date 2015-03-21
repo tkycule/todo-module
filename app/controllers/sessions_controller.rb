7d98233ab4d753b01f5b9eeba51db625a2f027df
tkycule <tky.cule@gmail.com>
2015-03-21 23:57:08 +0900

    sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,34 @@
 class SessionsController < ApplicationController
+
+  skip_before_filter :require_login, except: [:destroy]
+  before_filter :skip_login, only: [:new]
+
   def new
+    @user = User.new
   end
 
   def create
+    if @user = login(params[:user][:email], params[:user][:password])
+      redirect_back_or_to(tasks_path, notice: 'ログインしました。')
+    else
+      flash.now[:alert] = 'ログインに失敗しました。'
+      @user = User.new
+      render action: 'new'
+    end
   end
 
   def destroy
+    logout
+    redirect_to(root_path, notice: 'ログアウトしました。')
   end
+
+  private
+
+  def skip_login
+    if current_user
+      redirect_to tasks_path
+      return false
+    end
+  end
+
 end
