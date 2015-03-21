d305760ded3a54fff8150262a1c8bc4e5ad02753
tkycule <tky.cule@gmail.com>
2015-03-21 20:12:17 +0900

    task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,28 @@
 class Task < ActiveRecord::Base
+
+  include AASM
+
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+
+  aasm do
+    state :inbox, initial: true
+    state :completed
+    state :deleted
+
+    event :complete do
+      transitions to: :completed
+    end
+
+    event :delete do
+      transitions to: :deleted
+    end
+
+    event :revert do
+      transitions to: :inbox
+    end
+  end
+  
 end
