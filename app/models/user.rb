5da697f99ce869f3c90314891a6aa9a33a02b340
tkycule <tky.cule@gmail.com>
2015-03-21 20:14:14 +0900

    has_many :tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_many :tasks
+
   validates :email, presence: true, uniqueness: true, email_format: {message: "無効なメールアドレスです"}
   validates :password, presence: true, length: {minimum: 8}, confirmation: true
   validates :password_confirmation, presence: true
